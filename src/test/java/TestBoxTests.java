3e8e5fcb43ac6374101f2d0f1aecc800eb92e6b4
Andrey <zizkun.av@list.ru>
2024-03-13 23:46:49 +0400

    Промежуточный результат.

--- src/test/java/TestBoxTests.java
+++ src/test/java/TestBoxTests.java
@@ -1,53 +1,66 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 
 public class TestBoxTests {
 
     @BeforeAll
     static void beforeAll(){
+        Configuration.baseUrl ="https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
-    void fillFormTest(){
+    void fillFormTest() {
 
-            open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
 
 
-
-        //Name1, Name2, Email, Mobile, Current Address, Subjects
+        //firstName, lastName, Email, Mobile, Current Address, Subjects - однотипные элменты с заполненем поля текстом.
         $("#firstName").setValue("Ivan");
         $("#lastName").setValue("Ivanov");
         $("#userEmail").setValue("Ivanov2904.I@list.ru");
-        $("#userNumber").setValue("89000000001");
+        $("#userNumber").setValue("89377999147");
         $("#currentAddress").setValue("Country, City, Street, House №1");
-        $("#dateOfBirthInput").setValue("MoreSubjects");
+
+        $("#uploadPicture").uploadFile(new File("src/png.jpg")); //Picture
+
+        //Выбор пола.
+        $("[for=\"gender-radio-1\"]").click();
 
 
-        //Gender
-        $(byText("Male")).click();
+        //Выбор года.
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $(byText("1990")).click();
 
 
-        //$("#hobbies-checkbox-2").click(""); //Hobbies
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(byText("September")).click();
+        //Выбор месяца.
 
 
-        $("#uploadPicture").uploadFromClasspath("test.jpg"); //Picture
 
-        //$("#").click(); //State and City1
-        //$("#").click(); //State and Cit2
+
+
+
 
         $("#submit").click(); //submit
 
 
-        $("#subjectsInput").shouldHave(Condition.text(""));
+        $("#subjectsInput").shouldHave(text("Ivan"));
     }
 
 }
