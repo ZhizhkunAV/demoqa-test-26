c7b236e2937a5fb5492c9850ff627f0523ecfda3
ZhizhkunAV <116073898+ZhizhkunAV@users.noreply.github.com>
2024-03-28 16:50:50 +0400

    Update CalendarComponent.java

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -6,10 +6,10 @@ import static com.codeborne.selenide.Selenide.$;
 public class CalendarComponent {
 
     public void setDate(String day, String month, String year){
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").click();
-        $("[value='1990']").click();
-        $(".react-datepicker__month").$(byText("15")).click();
-    }
 
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__month").$(byText(day)).click();
+    }
 }
