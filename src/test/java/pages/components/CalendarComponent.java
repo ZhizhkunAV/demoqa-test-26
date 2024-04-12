47825b7f6f10afc84dfad0e4f26ef0ea4de7d569
Andrey_Zhizhkun <zizkun.av@list.ru>
2024-04-12 22:52:37 +0400

    Правки по календарю.

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -7,17 +7,21 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
 
-    //$("#dateOfBirthInput").click(); Прошлый вариант. Оставил для своего удобства.
+    //$("#dateOfBirthInput").click(); Прошлый вариант. Оставил для своего удобства. 1 вариант.
     //$(".react-datepicker__month-select").selectOption(month);
     // $(".react-datepicker__year-select").selectOption(year);
     //$(".react-datepicker__month").$(byText(day)).click();
 
     public void setDate(String day, String month, String year) {
-        $(".react-datepicker__year-select").selectOption(year);
+        //       $(".react-datepicker__year-select").selectOption(year); 2 вариант.
+        //       $(".react-datepicker__month-select").selectOption(month);
+        //       $(".react-datepicker__month")
+        //               .$$(byText(day))
+        //               .find(not(cssClass("react-datepicker__day--outside-month")))
+        //               .click();
+
         $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__month")
-                .$$(byText(day))
-                .find(not(cssClass("react-datepicker__day--outside-month")))
-                .click();
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0"+day+":not(.react-datepicker__day--outside-month)").click();
     }
 }
