80d1f54f22bb8385408e1ad85faa8c65858a65f9
Andrey_Zhizhkun <zizkun.av@list.ru>
2024-04-22 22:05:05 +0400

    Добавлен allure и интеграция с jenkins.

--- src/test/java/tests/registrations/TestBase.java
+++ src/test/java/tests/registrations/TestBase.java
@@ -3,9 +3,15 @@ package tests.registrations;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 
 public class TestBase {
@@ -16,8 +22,18 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.reportsFolder = System.getProperty("java.io.tmpdir");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
+
 
     @AfterEach
     void afterEach() {
@@ -33,4 +49,5 @@ public class TestBase {
             Attach.browserConsoleLogs();
         }
     }
+
 }
